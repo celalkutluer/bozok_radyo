9dc474d5601d449ddb033a219f27c1186c4cf2b9
CELAL KUTLUER <57915975+celalkutluer@users.noreply.github.com>
2020-08-20 21:08:59 +0300

    AndroidManifest.xml de güncelleme yapılarak uygulamalar içinde göreceğimiz iconlar düzenlendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
-
 import 'package:flutter_radio/flutter_radio.dart';
 
 void main() => runApp(new MyApp());
@@ -16,7 +14,7 @@ class _MyAppState extends State<MyApp> {
       "http://radyotelevizyon2.bozok.edu.tr:8030/;stream.mp3";
 
   bool isPlaying;
-  double radioVolume = 70;
+  bool isLoad = false;
 
   @override
   void initState() {
@@ -32,12 +30,13 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return new MaterialApp(title: 'Bozok FM',
+      theme: ThemeData(primaryColor: Colors.red[700]),
       debugShowCheckedModeBanner: false,
       home: new Scaffold(
         appBar: new AppBar(
-          backgroundColor: Colors.red[700],
           title: const Text('Bozok FM'),
+          backgroundColor: Colors.red[700],
         ),
         body: Padding(
           padding: const EdgeInsets.all(8.0),
@@ -53,49 +52,21 @@ class _MyAppState extends State<MyApp> {
                   fit: BoxFit.contain,
                 ),
               )),
-              Row(
-                children: <Widget>[
-                  FlatButton(
-                    child: Icon(
-                      Icons.pause_circle_filled,
-                      size: 66,
-                    ),
-                    onPressed: () {
-                      FlutterRadio.playOrPause(url: streamUrl);
-                      playingStatus();
-                    },
-                  ),
-                  FlatButton(
-                    child: Icon(
-                      Icons.play_circle_filled,
-                      size: 66,
-                      color:Colors.red[700]
-                    ),
-                    onPressed: () {
-                      FlutterRadio.playOrPause(url: streamUrl);
-                      playingStatus();
-                    },
-                  ),
-                  FlatButton(
-                    child: Icon(
-                      Icons.stop,
-                      size: 66,
-                    ),
-                    onPressed: () {
-                      FlutterRadio.playOrPause(url: streamUrl);
-                      playingStatus();
-                    },
-                  ),
-                ],
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              FlatButton(
+                child: Icon(
+                  isLoad ?  Icons.pause_circle_filled:Icons.play_circle_filled ,
+                  size: 100,
+                  color: isLoad ? Colors.black : Colors.red[700],
+                ),
+                onPressed: () {
+                  isLoad = !isLoad;
+                  FlutterRadio.playOrPause(url: streamUrl);
+                  playingStatus();
+                },
               ),
               SizedBox(
                 height: 10,
-              ), /*
-              Text(
-                'Check Playback Status: $isPlaying',
-                style: TextStyle(fontSize: 25.0),
-              )*/
+              ),
             ],
           )),
         ),
@@ -110,3 +81,49 @@ class _MyAppState extends State<MyApp> {
     });
   }
 }
+/*Row(
+                children: <Widget>[
+
+                 /* FlatButton(
+                    child: Icon(
+                      Icons.pause_circle_filled,
+                      size: 66,
+                      color: isLoad ? Colors.red[700]:Colors.black,
+                    ),
+                    onPressed: () {
+                      isLoad=!isLoad;
+                      FlutterRadio.playOrPause(url: streamUrl);
+                      playingStatus();
+                    },
+                  ),
+                  FlatButton(
+                    child: Icon(
+                      Icons.play_circle_filled,
+                      size: 66,
+                      color: isLoad ? Colors.red[700]:Colors.black,
+                    ),
+                    onPressed: () {
+                      isLoad=!isLoad;
+                      FlutterRadio.playOrPause(url: streamUrl);
+                      playingStatus();
+                    },
+                  ),
+                  FlatButton(
+                    child: Icon(
+                      Icons.stop,
+                      size: 66,
+                      color: isLoad ? Colors.red[700]:Colors.black,
+                    ),
+                    onPressed: () {
+                      isLoad=!isLoad;
+                      FlutterRadio.playOrPause(url: streamUrl);
+                      playingStatus();
+                    },
+                  ),*/
+                ],
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              ),*/
+/*Text(
+                'Check Playback Status: $isPlaying',
+                style: TextStyle(fontSize: 25.0),
+              )*/
